967b4016e4479bea73856d42d44917fedf12afca
Chamindu Hansana <chamindu553@gmail.com>
2025-06-10 21:06:08 +0530

    Update Lesson_15_WeightConverter.java

--- src/Lesson_15_WeightConverter.java
+++ src/Lesson_15_WeightConverter.java
@@ -26,14 +26,28 @@ public class Lesson_15_WeightConverter {
         // option 1 convert lbs to kgs
 
         if(choice == 1){
-            System.out.println("Enter the weight in lbs : ");
+            System.out.print("Enter the weight in lbs : ");
             weight = scanner.nextDouble();
-
-        }
+            newWeight = weight * 0.453592;
+            System.out.printf("The new weight in kgs : %.2f kg", newWeight);
+         }
 
         // option 2 convert kgs to lbs
 
+        if(choice == 2){
+            System.out.print("Enter the weight in kgs : ");
+            weight = scanner.nextDouble();
+            newWeight = weight * 2.20462;
+            System.out.printf("The new weight in lbs : %.2f lbs", newWeight);
+        }
+
         //else print not a valid choice
+
+        else{
+            System.out.println("That was not a valid choice.");
+        }
+
+        scanner.close();
 
     }
 }
